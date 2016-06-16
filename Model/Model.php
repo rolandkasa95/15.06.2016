8e95239926dd27d64052884fb595b41906084fab
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-16 12:03:26 +0300

    Create: login.php, model.php, config.php

---
+++ Model/Model.php
@@ -0,0 +1,9 @@
+<?php
+
+class Model
+{
+
+    public static $pdo;
+    $config = require 'Config/config.php';
+
+}
---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return ['db' => [
+            'dsn' => 'mysql:host=127.0.0.1;dbname=site',
+            'user' => 'admin',
+            'pass' => 'test']
+        ];
